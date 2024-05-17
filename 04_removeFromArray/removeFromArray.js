ded4002f38088d4926c5c7c7ee313b79816339f2
ElectronDragon <electricdragondevelopment@gmail.com>
2024-05-17 15:08:13 +0100

    Create if statement to skip push when array content equals exclude argument

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,13 @@
-const removeFromArray = function(array) {
+const removeFromArray = function(array, exclude) {
     let newArray = [9];
     let arrayLength = array.length;
 
     for (let i = 0; i < array.length; i++) {
+        if (array[i] == exclude) {
+            // do nothing
+        } else {
         newArray.push(array[i]);
+        }
     }
 
     // Need for loop and if statement for removal
@@ -11,7 +15,7 @@ const removeFromArray = function(array) {
     return newArray;
 };
 
-removeFromArray([1,2,3]);
+removeFromArray([1,2,3], 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
