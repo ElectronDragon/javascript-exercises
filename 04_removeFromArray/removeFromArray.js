cac503ef9a0e69b0d01193908d52b3d2b67752cb
ElectronDragon <electricdragondevelopment@gmail.com>
2024-05-17 16:30:09 +0100

    Create excludeArray from arguments with ...spread syntax and compare to array to make newArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,21 +1,38 @@
-const removeFromArray = function(array, exclude) {
-    let newArray = [9];
-    let arrayLength = array.length;
-
+const removeFromArray = function(array) {
+    let newArray = [];    
+    let excludeArray = [...arguments];
+    // This is necessary to separate array from the exclusions
+    excludeArray.shift();
+    
     for (let i = 0; i < array.length; i++) {
-        if (array[i] == exclude) {
-            // do nothing
+        if (excludeArray.includes(array[i])) {
+        // do nothing
         } else {
         newArray.push(array[i]);
         }
     }
 
-    // Need for loop and if statement for removal
-
     return newArray;
 };
 
-removeFromArray([1,2,3], 2);
+removeFromArray([1,2,3,4], 3, 4);
+
+// const removeFromArray = function(array, exclude) {
+    // let newArray = [];
+    // let arrayLength = array.length;
+    // let argLength = arguments.length;
+// 
+    // for (let i = 0; i < array.length; i++) {
+        // if (array[i] == exclude) {
+            do nothing
+        // } else {
+        // newArray.push(array[i]);
+        // }
+    // }
+// 
+    // return argLength;
+// };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
