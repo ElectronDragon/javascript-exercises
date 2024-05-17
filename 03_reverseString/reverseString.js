463771339025773acb1cc7ac8c71038c44249b14
ElectronDragon <electricdragondevelopment@gmail.com>
2024-05-17 13:55:46 +0100

    Create basic dupilication functionality in reverseString()

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,14 @@
 const reverseString = function(string) {
-    let newString = "test"
+    let newString = "A"
     let stringLength = string.length;
-    return stringLength;
+
+    for ( let i = 0 ; i < stringLength; i++) {
+        newString += string.charAt(i);
+    }
+    return newString;
 };
 
-reverseString("test");
+reverseString("tester");
 
 // Do not edit below this line
 module.exports = reverseString;
