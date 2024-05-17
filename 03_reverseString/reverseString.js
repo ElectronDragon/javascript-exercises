9d7dbfd3f92a721f52c9939410434d02a94bd1af
ElectronDragon <electricdragondevelopment@gmail.com>
2024-05-17 14:13:37 +0100

    Add comments after overcoming decrementing issue

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -8,7 +8,9 @@ const reverseString = function(string) {
     return newString;
 };
 
-reverseString();
+// Note to self: my initial difficulties arose from trying to make stringLength negative and working with i starting at -1 and working (down) towards stringLength. The code does NOT like this. Instead, stay positive, and work from stringLength -1 down to zero.
+
+reverseString("hello");
 
 // Do not edit below this line
 module.exports = reverseString;
