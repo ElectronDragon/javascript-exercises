04a865f063f2c7233d1b675bd76e678a020a0dfa
ElectronDragon <electricdragondevelopment@gmail.com>
2024-05-17 13:51:26 +0100

    Start adding arguments and internal constants to reverseString()

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newString = "test"
+    let stringLength = string.length;
+    return stringLength;
 };
+
+reverseString("test");
 
 // Do not edit below this line
 module.exports = reverseString;
