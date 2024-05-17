017d0abef019c0b0c6627c15888a4e510ad5b396
ElectronDragon <electricdragondevelopment@gmail.com>
2024-05-17 14:47:05 +0100

    Create newArray return for removeFromArray()

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let newArray = array;
 
+    return newArray;
 };
+
+removeFromArray([1,2,3]);
 
 // Do not edit below this line
 module.exports = removeFromArray;
