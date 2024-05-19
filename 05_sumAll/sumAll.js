c05b75c20ca3672780d2e86b947bff61a21dc32c
ElectronDragon <electricdragondevelopment@gmail.com>
2024-05-19 11:41:33 +0100

    Create function that passes first test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,16 +1,31 @@
 const sumAll = function(num1, num2) {
+    let smallerNumber
+    let largerNumber
     let smallSum
     let largeSum
     let intervalSum
     
+
     if (num1 < 1 || num2 < 1) {
         return "ERROR";
     } else {
-        return "good"
+        if (num1 > num2) {
+            largerNumber = num1;
+            smallerNumber = num2;
+        } else {
+            largerNumber = num2;
+            smallerNumber = num1;
+        }
+
+        smallSum = smallerNumber*(smallerNumber+1)/2;
+        largeSum = largerNumber*(largerNumber+1)/2;
+        intervalSum = largeSum-smallSum +1;
+
+        return intervalSum;
     }
 };
 
-sumAll(1,-2);
+sumAll(1,4);
 
 // Gaussian distribution = (n/2)(first num + last num) where n is the number of intergers
 
