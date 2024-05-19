351c03be1bf80558e63a4b0506c55ca51396ae69
ElectronDragon <electricdragondevelopment@gmail.com>
2024-05-19 12:24:43 +0100

    Add formula and rounding to fuctions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let tempCelsius = (temp-32)*5/9;
+  let tempCelsiusRounded = Math.round(tempCelsius*10)/10;
+  return tempCelsiusRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let tempFahrenheit = (temp * 9/5) +32;
+  let tempFahrenheitRounded = Math.round(tempFahrenheit*10)/10;
+  return tempFahrenheitRounded;
 };
 
 // Do not edit below this line
