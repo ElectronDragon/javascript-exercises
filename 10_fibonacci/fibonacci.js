46c2e59f1844b847f823870d538355bbbe82777a
ElectronDragon <electricdragondevelopment@gmail.com>
2024-05-28 09:21:00 +0100

    Create solution and comments

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(rawNum) {
+    if (rawNum < 0) {
+        return "OOPS"
+    } else {
+    let num = parseInt(rawNum);
+    let squareFive = Math.sqrt(5);
+    let fibNum = Math.round((((1+squareFive)**num)-((1-squareFive)**num))/((2**num)*squareFive));
 
+    return fibNum;
+    }
 };
+
+// My solution was very different simply because I just ed used the fibonacci formula rather than creating a loop.
 
 // Do not edit below this line
 module.exports = fibonacci;
