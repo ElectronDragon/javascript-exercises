0373b0f3f4ca3d971db9a75866e4a93a52e3fcb9
ElectronDragon <electricdragondevelopment@gmail.com>
2024-05-28 10:17:03 +0100

    Create solution and comment

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    let titleList = books.map(i => i.title);
+
+    return titleList;
 
 };
+
+//  I needed a lot of help with this one. I don't think I understand .map as well as I need to, but it's slowly coming together.
 
 // Do not edit below this line
 module.exports = getTheTitles;
